ca4ad513387fce6d224dbfa975897fe515cd548e
eedomeng <eedomeng@gmail.com>
2022-12-19 11:53:46 +0900

    404메세지

--- QRServer/src/com/mc/qrserver/http/HttpStatusCode.java
+++ QRServer/src/com/mc/qrserver/http/HttpStatusCode.java
@@ -1,28 +1,29 @@
 package com.mc.qrserver.http;
 
 public enum HttpStatusCode {
-	OK(200, "ok", "http/1.1 200 ok");
+	OK(200, "ok", "http/1.1 200 ok \n"),
+	NOT_FOUND(404, "Not Found", "http/1.1 404 Not Found \n");
 	
 	private int code;
 	private String message;
-	private String start_line;
+	private String startLine;
 	
 	private HttpStatusCode(int code, String message, String start_line) {
 		this.code = code;
 		this.message = message;
-		this.start_line = start_line;
+		this.startLine = start_line;
 	}
 
 	public int getCode() {
 		return code;
 	}
 
-	public void setCode(int code) {
-		this.code = code;
-	}
-
 	public String getMessage() {
 		return message;
 	}
-	
+
+	public String getStartLine() {
+		return startLine;
+	}
+
 }
