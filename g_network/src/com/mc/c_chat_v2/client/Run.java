d2006bc4526d9b9ca94963dfbda40f8477ca99be
eedomeng <eedomeng@gmail.com>
2022-12-20 18:05:15 +0900

    유저등록

---
+++ g_network/src/com/mc/c_chat_v2/client/Run.java
@@ -0,0 +1,10 @@
+package com.mc.c_chat_v2.client;
+
+public class Run {
+
+	public static void main(String[] args) {
+		new ChatClient().startChat();
+
+	}
+
+}
---
+++ g_network/src/com/mc/c_chat_v2/server/Run.java
@@ -0,0 +1,10 @@
+package com.mc.c_chat_v2.server;
+
+public class Run {
+
+	public static void main(String[] args) {
+		new ChatServer().startServer();
+
+	}
+
+}
