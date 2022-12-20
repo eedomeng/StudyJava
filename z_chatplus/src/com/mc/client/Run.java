172ce500d07e71e97db30549500c0f481bacf441
eedomeng <eedomeng@gmail.com>
2022-12-20 10:36:38 +0900

    기본 전체채팅

---
+++ z_chatplus/src/com/mc/client/Run.java
@@ -0,0 +1,10 @@
+package com.mc.client;
+
+public class Run {
+
+	public static void main(String[] args) {
+		new ChatClient().startChat();
+
+	}
+
+}
---
+++ z_chatplus/src/com/mc/server/Run.java
@@ -0,0 +1,10 @@
+package com.mc.server;
+
+public class Run {
+
+	public static void main(String[] args) {
+		new ChatServer().startServer();
+
+	}
+
+}
